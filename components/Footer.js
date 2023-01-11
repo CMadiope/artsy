9ea4046634f5cc3e9ca0dcd743c0fe2abebb73c7
Confidence Madiope <confidencem97@gmail.com>
2023-01-11 13:18:30 +0200

    ADD: drop section

--- components/Footer.js
+++ components/Footer.js
@@ -5,57 +5,61 @@ import { GoLocation } from "react-icons/go";
 
 const Footer = () => {
   return (
-    <footer className="pt-56">
-      <div className=" text-center pt-10 border pb-10 ">
-        <h1 className="font-baskervville self-center lg:text-xl">NEWSLETTER</h1>
-        <p className="lg:text-lg">Subscribe to get daily updates on new drops & exciting deals </p>
-        <div className="pt-3 font-baskervville ">
+    <footer className='pt-56'>
+      <div className=' text-center pt-10 border pb-10 '>
+        <h1 className='font-baskervville self-center lg:text-xl'>NEWSLETTER</h1>
+        <p className='lg:text-lg'>
+          Subscribe to get daily updates on new drops & exciting deals{" "}
+        </p>
+        <div className='pt-3 font-baskervville '>
           <input
-            type="text"
-            placeholder="Enter your email"
-            className="p-2 border outline-none pl-8 text-sm"
+            type='text'
+            placeholder='Enter your email'
+            className='p-2 border outline-none pl-8 text-sm'
           />
-          <button className="p-2 bg-neutral-800 ml-3 pl-4 pr-4 text-white text-sm hover:bg-neutral-500 hover:text-bg-slate-200">
+          <button className='p-2 bg-neutral-800 ml-3 pl-4 pr-4 text-white text-sm hover:bg-neutral-500 hover:text-bg-slate-200'>
             SUBSCRIBE
           </button>
         </div>
       </div>
-      <div className="pt-10 mb-20">
-        <div className="flex justify-between">
+      <div className='pt-10 mb-20'>
+        <div className='flex justify-between'>
           <div>
-            <Link href="/">
-              <h1 className="text-2xl font-bold font-stix_two lg:text-4xl">ARTSY.</h1>
+            <Link href='/'>
+              <h1 className='text-2xl font-bold font-stix_two lg:text-4xl'>
+                ARTSY.
+              </h1>
             </Link>
           </div>
           <div>
-            <ul className="flex flex-col lg:text-lg">
-              <Link href="/">Home</Link>
-              <Link href="/marketplace">Marketplace</Link>
-              <Link href="auctions">Auctions</Link>
-              <Link href="/drops">Drops</Link>
+            <ul className='flex flex-col lg:text-lg'>
+              <Link href='/'>Home</Link>
+              <Link href='/marketplace'>Marketplace</Link>
+              <Link href='auctions'>Auctions</Link>
+              <Link href='/drop'>Drops</Link>
             </ul>
           </div>
           <div>
-            <ul className="flex flex-col lg:text-lg">
-              <Link href="#">Blog</Link>
-              <Link href="#">Wallets</Link>
-              <Link href="#">Rates</Link>
-              <Link href="#">High bids</Link>
+            <ul className='flex flex-col lg:text-lg'>
+              <Link href='#'>Blog</Link>
+              <Link href='#'>Wallets</Link>
+              <Link href='#'>Rates</Link>
+              <Link href='#'>High bids</Link>
             </ul>
           </div>
           <div>
-            <div className="flex items-center gap-2 text-lg cursor-pointer lg:text-xl">
+            <div className='flex items-center gap-2 text-lg cursor-pointer lg:text-xl'>
               <AiOutlineMail />
               <p>artsystudios@gmail.com</p>
             </div>
-            <div className="flex items-center gap-2 text-lg cursor-pointer">
+            <div className='flex items-center gap-2 text-lg cursor-pointer'>
               <GoLocation />
               <p>Johannesburg, South Africa</p>
             </div>
           </div>
         </div>
-        <div className="text-center p-10">
-          <p className="text-sm font-bold font-rubik lg:text-base">
+        <div className='text-center p-10'>
+          <p className='text-sm font-bold font-rubik lg:text-base'>
             Artsystudios © 2022. All Rights Reserved.
           </p>
         </div>
--- pages/drop.js
+++ pages/drop.js
@@ -1,9 +1,173 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import ghost from "../public/drop/ghost.png";
+import woods from '../public/drop/woods.png'
+import water from '../public/drop/water.png'
+import warped from '../public/drop/warped.png'
 
 const drop = () => {
   return (
-    <div>drop</div>
-  )
-}
+    <div className='mx-auto'>
+      <h1 className='text-center text-xl sm:text-2xl  md:text-3xl font-bold sm:font-semibold md:font-normal pt-8 sm:pt-12'>
+        Upcoming drops
+      </h1>
+      <p className='text-center text-sm  sm:text-base md:lg pt-4 md:pt-6 text-gray-500'>
+        Turn on notifications so that no drops will miss you.
+      </p>
+      <div className='flex justify-center pt-8 '>
+        <button className='py-2  sm:px-16 px-12 border border-black rounded-md md:text-lg hover:bg-gray-100'>
+          Notify me
+        </button>
+      </div>
+      <div className='flex flex-col justify-center pt-8 md:pt-12'>
+        <div className='flex flex-col items-center md:flex-row'>
+          <div className='relative'>
+            <Image src={ghost} alt='/' />
+            <div className='absolute bottom-0 text-white flex gap-8 items-center py-6 px-8 bg-white/20 rounded-lg ml-4 mb-6'>
+              <div>
+                <p>Time remaining</p>
+                <h1 className='text-xl font-stix_two'>
+                  06 hrs : 45 min : 22 s
+                </h1>
+              </div>
+              <button className=' bg-blue-400 py-px px-4 rounded-xl hover:bg-blue-300'>
+                Join
+              </button>
+            </div>
+          </div>
+          <div className='pt-6  md:pt-0 md:pl-8 max-md:text-center'>
+            <button className='py-1 px-6 bg-blue-400 rounded-lg text-white hover:bg-blue-300 uppercase font-inter md:text-lg md:py-2 md:px-8'>
+              upcoming
+            </button>
+            <p className='text-sm text-gray-500 pt-4 md:text-base'>
+              November 21 at 11 am WAT
+            </p>
+            <h3 className='font-semibold text-xl pt-4 md:text-2xl'>
+              Eyo : Eko For Show
+            </h3>
+            <p className='text-xs md:text-sm text-gray-500 pt-4'>
+              Lorem ipsum dolor sit amet consectetur. Amet odio a aenean quis
+              vitae tempus. Sed nunc tempus aliquet lectus ut vulputate.
+            </p>
+            <p className='pt-4 md:text-lg'>
+              Creator : <span className='text-blue-400'>Aliya Minat</span>
+            </p>
+            <p className='text-blue-400 underline cursor-pointer pt-4 md:text-lg'>
+              Get notified
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center md:flex-row max-md:pt-12 md:pt-20'>
+          <div className='relative'>
+            <Image src={woods} alt='/' />
+            <div className='absolute bottom-0 text-white flex gap-8 items-center py-6 px-8 bg-white/20 rounded-lg ml-4 mb-6'>
+              <div>
+                <p>Time remaining</p>
+                <h1 className='text-xl font-stix_two'>
+                  06 hrs : 45 min : 22 s
+                </h1>
+              </div>
+              <button className=' bg-blue-400 py-px px-4 rounded-xl hover:bg-blue-300'>
+                Join
+              </button>
+            </div>
+          </div>
+          <div className='pt-6  md:pt-0 md:pl-8 max-md:text-center'>
+            <button className='py-1 px-6 bg-green-400 rounded-lg text-white hover:bg-green-300 uppercase font-inter md:text-lg md:py-2 md:px-8'>
+              live now
+            </button>
+            <p className='text-sm text-gray-500 pt-4 md:text-base'>
+              November 21 at 11 am WAT
+            </p>
+            <h3 className='font-semibold text-xl pt-4 md:text-2xl'>
+              Ginger Suburbs
+            </h3>
+            <p className='text-xs md:text-sm text-gray-500 pt-4'>
+              Lorem ipsum dolor sit amet consectetur. Amet odio a aenean quis
+              vitae tempus. Sed nunc tempus aliquet lectus ut vulputate.
+            </p>
+            <p className='pt-4 md:text-lg'>
+              Creator : <span className='text-blue-400'>Tina Benson</span>
+            </p>
+            <p className='text-blue-400 underline cursor-pointer pt-4 md:text-lg'>
+              Join now
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center md:flex-row max-md:pt-12 md:pt-20'>
+          <div className='relative'>
+            <Image src={water} alt='/' />
+            <div className='absolute bottom-0 text-white flex gap-8 items-center py-6 px-8 bg-white/20 rounded-lg ml-4 mb-6'>
+              <div>
+                <p>Auction ended</p>
+                <h1 className='text-xl font-stix_two'>2 hours ago</h1>
+              </div>
+              <button className=' bg-gray-400 py-px px-4 rounded-xl hover:bg-gray-300'>
+                view
+              </button>
+            </div>
+          </div>
+          <div className='pt-6  md:pt-0 md:pl-8 max-md:text-center'>
+            <button className='py-1 px-6 bg-gray-400 rounded-lg text-white hover:bg-gray-300 uppercase font-inter md:text-lg md:py-2 md:px-8'>
+              ended
+            </button>
+            <p className='text-sm text-gray-500 pt-4 md:text-base'>
+              November 21 at 11 am WAT
+            </p>
+            <h3 className='font-semibold text-xl pt-4 md:text-2xl'>Sink</h3>
+            <p className='text-xs md:text-sm text-gray-500 pt-4'>
+              Lorem ipsum dolor sit amet consectetur. Amet odio a aenean quis
+              vitae tempus. Sed nunc tempus aliquet lectus ut vulputate.
+            </p>
+            <p className='pt-4 md:text-lg'>
+              Creator : <span className='text-blue-400'>Aliya Minat</span>
+            </p>
+            <p className='text-blue-400 underline cursor-pointer pt-4 md:text-lg'>
+              View
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center md:flex-row max-md:pt-12 md:pt-20'>
+          <div className='relative'>
+            <Image src={warped} alt='/' />
+            <div className='absolute bottom-0 text-white flex gap-8 items-center py-6 px-8 bg-white/20 rounded-lg ml-4 mb-6'>
+              <div>
+                <p>Auction ended</p>
+                <h1 className='text-xl font-stix_two'>5 hours ago</h1>
+              </div>
+              <button className=' bg-gray-400 py-px px-4 rounded-xl hover:bg-gray-300'>
+                view
+              </button>
+            </div>
+          </div>
+          <div className='pt-6  md:pt-0 md:pl-8 max-md:text-center'>
+            <button className='py-1 px-6 bg-gray-400 rounded-lg text-white hover:bg-gray-300 uppercase font-inter md:text-lg md:py-2 md:px-8'>
+              ended
+            </button>
+            <p className='text-sm text-gray-500 pt-4 md:text-base'>
+              November 21 at 11 am WAT
+            </p>
+            <h3 className='font-semibold text-xl pt-4 md:text-2xl'>
+              Warped ‘99
+            </h3>
+            <p className='text-xs md:text-sm text-gray-500 pt-4'>
+              Lorem ipsum dolor sit amet consectetur. Amet odio a aenean quis
+              vitae tempus. Sed nunc tempus aliquet lectus ut vulputate.
+            </p>
+            <p className='pt-4 md:text-lg'>
+              Creator : <span className='text-blue-400'>Aliya Minat</span>
+            </p>
+            <p className='text-blue-400 underline cursor-pointer pt-4 md:text-lg'>
+              View
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center pt-8 md:pt-16">
+        <button className="py-2 px-12 rounded-md border border-black hover:bg-slate-50 md:text-lg md:px-16">See more</button>
+      </div>
+    </div>
+  );
+};
 
-export default drop
+export default drop;
