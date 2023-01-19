30ef44fe872031ab7dd78d43754fc40293ae6e03
Confidence Madiope <confidencem97@gmail.com>
2023-01-19 19:02:13 +0200

    fix:footer mobile response

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,14 @@ import React from "react";
 import Link from "next/link";
 import { AiOutlineMail } from "react-icons/ai";
 import { GoLocation } from "react-icons/go";
+import { useRouter } from "next/router";
 
 const Footer = () => {
+
+  const router = useRouter()
+// 
   return (
-    <footer className='pt-56 '>
+    <footer className={router.pathname == '/confirm'? 'hidden': 'pt-56'}>
       <div className=' sm:text-center sm:pt-10 sm:border sm:pb-10 '>
         <h1 className='font-baskervville text-xl md:text-2xl self-center lg:text-4xl max-sm:text-left pb-4'>NEWSLETTER</h1>
         <p className='lg:text-lg text-gray-500'>
@@ -23,8 +27,8 @@ const Footer = () => {
         </div>
       </div>
       <div className='pt-10 mb-20'>
-        <div className='flex justify-between'>
-          <div>
+        <div className='flex max-sm:flex-col max-sm:gap-4 justify-between'>
+          <div className="max-sm:hidden">
             <Link href='/'>
               <h1 className='text-2xl font-bold font-stix_two lg:text-4xl'>
                 ARTSY.
@@ -39,7 +43,7 @@ const Footer = () => {
               <Link href='/drop'>Drops</Link>
             </ul>
           </div>
-          <div>
+          <div className="max-sm:hidden">
             <ul className='flex flex-col lg:text-lg'>
               <Link href='#'>Blog</Link>
               <Link href='#'>Wallets</Link>
